aa99a14db4b9012c6a940310efdf9f692ab1431b
Doriane97 <doriane.bedier974@gmail.com>
2021-01-02 15:00:16 +0100

    Changmeent des noms dans serveur_udp

--- serveur_udp.c
+++ serveur_udp.c
@@ -146,41 +146,31 @@ int main(int argc, char *argv[])
 #include <ctype.h>
 
 int main(){
-  int udpSocket, nBytes;
+  int sockfd, nBytes;
   char buffer[1024];
   struct sockaddr_in serverAddr, clientAddr;
   struct sockaddr_storage serverStorage;
   socklen_t addr_size, client_addr_size;
   int i;
 
-  /*Create UDP socket*/
-  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 
-  /*Configure settings in address struct*/
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(7891);
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*Bind socket with address struct*/
-  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-  /*Initialize size variable to be used later on*/
   addr_size = sizeof serverStorage;
 
-  while(1){
-    /* Try to receive any incoming UDP datagram. Address and port of
-      requesting client will be stored on serverStorage variable */
-    nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
-
-    /*AFFicger dans le serveur le message recu*/
-    /*for(i=0;i<nBytes-1;i++)
-      buffer[i] = toupper(buffer[i]);*/
+  int j = 0;
+  while(j<20){
+    j++;
+    nBytes = recvfrom(sockfd,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
 
     printf("Received from client: %s\n",buffer);
 
-    /*Send uppercase message back to client, using serverStorage as the address*/
-    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
   }
 
   return 0;
