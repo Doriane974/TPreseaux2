401a27ed6b117c5ec3e35c4e08fe0fb6090d3355
MargueriteLaVache <76214238+MargueriteLaVache@users.noreply.github.com>
2021-01-03 20:03:47 +0100

    Update serveur_udp.c
    Messages en français et indentation.

--- serveur_udp.c
+++ serveur_udp.c
@@ -9,31 +9,31 @@
 #define PORT 9600
 
 int main(){
-  int sockfd, nBytes;
-  char buffer[MAX];
-  struct sockaddr_in serverAddr, clientAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size, client_addr_size;
-  int i;
+	int sockfd, nBytes;
+	char buffer[MAX];
+	struct sockaddr_in serverAddr, clientAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size, client_addr_size;
+	int i;
 
-  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(PORT);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-  addr_size = sizeof serverStorage;
+	addr_size = sizeof serverStorage;
 
-  for(;;){
-    nBytes = recvfrom(sockfd,buffer,MAX,0,(struct sockaddr *)&serverStorage, &addr_size);
-    if ((strncmp(buffer, "exit", 4)) == 0) {
-			printf("Client Exit...\n");
+	for(;;){
+		nBytes = recvfrom(sockfd,buffer,MAX,0,(struct sockaddr *)&serverStorage, &addr_size);
+		if ((strncmp(buffer, "exit", 4)) == 0) {
+			printf("Fermeture du client...\n");
 			break;
 		}
-    printf("Received from client: %s\n",buffer);
-  }
-  return 0;
+		printf("Recu du client: %s\n",buffer);
+	}
+	return 0;
 }
