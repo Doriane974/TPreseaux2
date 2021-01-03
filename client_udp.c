3ca02dbfe763f5f479d4a91480c6d593daeaab42
MargueriteLaVache <76214238+MargueriteLaVache@users.noreply.github.com>
2021-01-03 19:56:08 +0100

    Update client_udp.c
    Messages en français et indentation.

--- client_udp.c
+++ client_udp.c
@@ -9,31 +9,31 @@
 
 int main(){
 
-  int sockfd, nBytes;
-  char buffer[MAX];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
+	int sockfd, nBytes;
+	char buffer[MAX];
+	struct sockaddr_in serverAddr;
+	socklen_t addr_size;
 
-  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(PORT);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  addr_size = sizeof serverAddr;
-  for(;;){
-    printf("Type a sentence to send to server:\n");
-    fgets(buffer,MAX+1,stdin);
-    printf("You typed: %s \n",buffer);
+	addr_size = sizeof serverAddr;
+	for(;;){
+		printf("Taper le message a envoyer au serveur :\n");
+		fgets(buffer,MAX+1,stdin);
+		printf("Vous avez taper : %s \n",buffer);
 
-    nBytes = strlen(buffer) + 1;
+		nBytes = strlen(buffer) + 1;
 
-    sendto(sockfd,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
-    if ((strncmp(buffer, "exit", 4)) == 0) {
-			printf("Client Exit...\n");
-			break;
-		}
-  }
-  return 0;
+		sendto(sockfd,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+		if ((strncmp(buffer, "exit", 4)) == 0) {
+				printf("Fermeture du client...\n");
+				break;
+			}
+	}
+  	return 0;
 }
